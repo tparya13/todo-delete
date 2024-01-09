2dae3e497dfab7b0418ffe1281a4680871f1b740
arya <aryatheruparambil@gmail.com>
2024-01-09 13:20:10 +0530

    file added

---
+++ TODO/ARYA/todo delete/Todoproject/TodoApp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render,redirect
+from .models import Task
+# Create your views here.
+def Home(req):
+    tasks=Task.objects.all()
+    if req.method=="POST":
+        task=req.POST["task"]
+        priority=req.POST["priority"]
+        todo=Task(task=task,priority=priority)
+        todo.save()
+    return render(req,'index.html',{"tasks":tasks})
+
+def Update(req,id):
+    tasks=Task.objects.get(id=id) 
+    if req.method=="POST":
+        task=req.POST.get('task','')
+        priority=req.POST.get('priority','')
+        Task.objects.filter(id=id).update(task=task,priority=priority)
+        return redirect("home")
+    return render(req,'update.html',{"task":tasks})       
+
+def Delete(req,id):
+    tasks=Task.objects.get(id=id) 
+    if req.method=="POST":
+        Task.objects.filter(id=id).delete()
+        return redirect("home")
+    return render(req,'delete.html',{"task":tasks})  
